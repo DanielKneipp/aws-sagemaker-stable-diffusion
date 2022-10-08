c53f492b9f898ddfbdbb139e93a63d9600f50a93
Daniel Kneipp <daniel.kneipp@outlook.com>
2022-10-08 23:57:29 +0200

    feat(aws): faster compression with pigz

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 asdf install
 
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt install git-lfs
+sudo apt install git-lfs pigz
 
 git lfs install
 git submodule update --init --recursive
